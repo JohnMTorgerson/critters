efcfa7dc54e84d24b78fd33a303d8529ee56f835
John Torgerson <john.m.torgerson@gmail.com>
2022-02-20 16:36:03 -0600

    began Predator class

---
+++ src/critters/Predator.js
@@ -0,0 +1,11 @@
+// -------- Predator class -------- //
+import Thinker from './Thinker.js'
+import NeuralNet from './helpers/NeuralNet.js';
+import MindReader from '../inspector/MindReader.js';
+
+export default class Predator extends Thinker {
+	constructor(canvas, gameOpts, params) {
+		super(canvas, gameOpts, params)
+
+	}
+}
